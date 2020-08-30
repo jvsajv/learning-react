46d69f1fb5bf55143600f4e9160c8931233000b1
Joao Almeida <runner.troll.rt@gmail.com>
2020-08-30 14:49:33 -0300

    feat(index/users): server now communicate with client side and server now manage the current users and rooms

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const {addUser, removeUsr, getUser, getUsersInRoom} = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -11,7 +13,30 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) =>{
-    console.log('New client connected!!!');
+
+    socket.on('join', ({ name, room }, callback) => {
+
+        const {error, user} = addUser({ id: socket.id, name, room})
+
+        if (error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, bem vindo a sala ${user.room}!`})
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} entrou na sala!`})
+
+        socket.join(user.room);
+
+        callback();
+
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
+
+        callback();
+    })
+
     socket.on('disconnect', () =>{
        console.log('Client disconnected!!!')
     });
