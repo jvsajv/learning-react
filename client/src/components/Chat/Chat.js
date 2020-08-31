bb27c7e313c987ee694ea8a3db1cfc37fdf19a15
Joao Almeida <runner.troll.rt@gmail.com>
2020-08-31 19:21:23 -0300

    feat(client): now the chat dosen't let anyone with an already created username enter in the respectvily room redirecting to the Join page

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
+import {Redirect} from "react-router-dom";
 
 import './Chat.css'
 import InfoBar from '../InfoBar/InfoBar'
@@ -8,6 +9,7 @@ import Input from '../Input/Input'
 import Messages from '../Messages/Messages'
 import TextContainer from '../TextContainer/TextContainer'
 import GameTab from '../GameTab/GameTab'
+import Join from '../Join/Join'
 
 let socket;
 
@@ -15,7 +17,7 @@ const Chat = ({location}) => {
 
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    const [user, setUser] = useState('');
+    const [isOk, setOk] = useState(true)
     const [userClass, setClass] = useState('');
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
@@ -30,8 +32,11 @@ const Chat = ({location}) => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', {name, room}, () => {
-
+        socket.emit('join', {name, room}, (error) => {
+            if (error) {
+                alert(error)
+                setOk(false)
+            }
         });
         socket.emit('getClass', () => {
 
@@ -47,14 +52,14 @@ const Chat = ({location}) => {
 
     useEffect(() => {
         socket.on('message', (message) => {
-            setMessages(messages => [ ...messages, message ]);
+            setMessages(messages => [...messages, message]);
         });
         socket.on("roomData", ({users}) => {
             setUsers(users);
         });
-        socket.on("getClass", (userClass) =>{
+        socket.on("getClass", (userClass) => {
             setClass(userClass)
-        })
+        });
     }, []);
 
     const sendMessage = (event) => {
@@ -68,20 +73,24 @@ const Chat = ({location}) => {
     }
 
     return (
-        <div className="outerContainer">
-            <div className="users-list">
-                <TextContainer users={users}/>
-            </div>
-            <div className="container">
-                <InfoBar room={room}/>
-                <Messages messages={messages} name={name}/>
-                <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
-            </div>
-            <div className="game-tab">
-                <GameTab userClass={userClass} name={name}/>
-            </div>
+        isOk ? (
+                <div className="outerContainer">
+                    <div className="users-list">
+                        <TextContainer users={users}/>
+                    </div>
+                    <div className="container">
+                        <InfoBar room={room}/>
+                        <Messages messages={messages} name={name}/>
+                        <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+                    </div>
+                    <div className="game-tab">
+                        <GameTab userClass={userClass} name={name}/>
+                    </div>
 
-        </div>
+                </div>
+            ) : (
+                <Redirect to="/"/>
+            )
     )
 }
 
